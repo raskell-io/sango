62ecd6af30b7e9df1419604461968b83ca054f88
raffaelschneider <raffael.schneider@protonmail.com>
2026-01-02 09:44:40 +0100

    Deploy site with prominent Ko-fi support section

--- wasm/sango_web.js
+++ wasm/sango_web.js
@@ -224,12 +224,12 @@ if (!('encodeInto' in cachedTextEncoder)) {
 
 let WASM_VECTOR_LEN = 0;
 
-function __wasm_bindgen_func_elem_692(arg0, arg1, arg2) {
-    wasm.__wasm_bindgen_func_elem_692(arg0, arg1, addHeapObject(arg2));
-}
-
 function __wasm_bindgen_func_elem_182(arg0, arg1) {
     wasm.__wasm_bindgen_func_elem_182(arg0, arg1);
+}
+
+function __wasm_bindgen_func_elem_692(arg0, arg1, arg2) {
+    wasm.__wasm_bindgen_func_elem_692(arg0, arg1, addHeapObject(arg2));
 }
 
 function __wasm_bindgen_func_elem_738(arg0, arg1, arg2, arg3) {
